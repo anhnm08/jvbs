b20e4677425874bbd659a57c776fd60802612acd
anhnm08 <anhnm08.gec@gmail.com>
2024-07-13 23:11:43 +0700

    edit class nhap

--- src/javabasic/nhap.java
+++ src/javabasic/nhap.java
@@ -1,4 +1,11 @@
 package javabasic;
 
 public class nhap {
+   static int n1 = 1;
+   static int n2 = 2;
+
+    public static void main(String[] args) {
+        System.out.println(n1+n2);
+    }
+
 }
